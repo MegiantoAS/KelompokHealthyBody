e048b2ef93a27b5fb971984645b2e99cc8ae419f
Megianto Adi Saputra <72857078+MegiantoAS@users.noreply.github.com>
2021-02-16 06:23:18 +0700

    workout

---
+++ workout/arm.php
@@ -0,0 +1,126 @@
+<?php require("navbar.php"); ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css ?v=1.2">
+    <title>Body Healthy | Workout</title>
+</head>
+
+<body>
+
+    <div id="judul">
+        <b>Arm Workout</b>
+    </div>
+    <style>
+        #judul {
+            text-align: center;
+            font-size: 25px;
+            font-family: 'Luckiest Guy', cursive;
+        }
+    </style>
+    <!-- content -->
+    <br>
+
+    <div class="container">
+        <class class="row">
+            <div class="col-md-4 mb-2">
+                <!-- card -->
+                <div class="card ">
+                    <iframe width="350" height="315" src="https://www.youtube.com/embed/v9LABVJzv8A" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="card-body card text-white bg-dark">
+                        <h3 class="card-text">Latihan#1 </h3>
+                        <h3 class="card-text">PushUp</h3>
+                        <h7 class="card-text">Sumber : https://youtu.be/v9LABVJzv8A
+                        </h7>
+                    </div>
+                </div>
+
+            </div>
+            <!-- akhir card -->
+
+            <div class="col-md-4 mb-4">
+                <!-- card -->
+                <div class="card">
+                    <iframe width="350" height="315" src="https://www.youtube.com/embed/M_4Vt5lfEUE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="card-body card text-white bg-dark">
+                        <h3 class="card-text">Latihan#2 </h3>
+                        <h3 class="card-text">Punch </h3>
+                        <h7 class="card-text">Sumber : https://youtu.be/M_4Vt5lfEUE
+                        </h7>
+                    </div>
+                </div>
+                <!-- akhir card -->
+            </div>
+            <div class="col-md-4 mb-4">
+                <!-- card -->
+                <div class="card">
+                    <iframe width="350" height="315" src="https://www.youtube.com/embed/sgc_gNStBqc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="card-body card text-white bg-dark">
+                        <h3 class="card-text">Latihan#3 </h3>
+                        <h3 class="card-text"> Thigh Tap</h3>
+                        <h7 class="card-text">Sumber : https://youtu.be/sgc_gNStBqcs
+                        </h7>
+                    </div>
+                </div>
+                <!-- akhir card -->
+            </div>
+            <div class="col-md-4 mb-4">
+                <!-- card -->
+                <div class="card">
+                    <iframe width="350" height="315" src="https://www.youtube.com/embed/gWHQpMUd7vw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="card-body card text-white bg-dark">
+                        <h3 class="card-text">Latihan#4 </h3>
+                        <h3 class="card-text">Shoulder Tap </h3>
+                        <h7 class="card-text">Sumber :https://youtu.be/gWHQpMUd7vw
+                        </h7>
+                    </div>
+                </div>
+                <!-- akhir card -->
+            </div>
+            <div class="col-md-4 mb-4">
+                <!-- card -->
+                <div class="card">
+                    <iframe width="350" height="315" src="https://www.youtube.com/embed/UPV4156lSyc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="card-body card text-white bg-dark">
+                        <h3 class="card-text">Latihan#5 </h3>
+                        <h3 class="card-text">Overhead punch </h3>
+                        <h7 class="card-text">Sumber : https://youtu.be/UPV4156lSyc
+                        </h7>
+                    </div>
+                </div>
+                <!-- akhir card -->
+            </div>
+            <div class="col-md-4 mb-4">
+                <!-- card -->
+                <div class="card">
+                    <iframe width="350" height="315" src="https://www.youtube.com/embed/KQUjYIgpoFw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="card-body card text-white bg-dark">
+                        <h3 class="card-text">Latihan#6 </h3>
+                        <h3 class="card-text">Tricep Extension</h3>
+                        <h7 class="card-text">Sumber : https://youtu.be/KQUjYIgpoFw
+                        </h7>
+                    </div>
+                </div>
+                <!-- akhir card -->
+            </div>
+
+            <!-- akhir content -->
+
+
+
+
+            <!-- Optional JavaScript -->
+            <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+            <script type="text/javascript" src="js/jquery-3.4.1.slim.min.js"></script>
+            <script type="text/javascript" src="js/popper.min.js"></script>
+            <script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ workout/body.php
@@ -0,0 +1,179 @@
+<?php require("navbar.php"); ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css?v=1.3">
+    <title>Workout | HealthyBody</title>
+    <link rel="icon" href="../image/tittle.png">
+</head>
+
+<body>
+    <div id="judul">
+        <b>Full Body Workout</b>
+    </div>
+    <style>
+        #judul {
+            text-align: center;
+            font-size: 25px;
+            font-family: 'Luckiest Guy', cursive;
+        }
+    </style>
+    <div class="container">
+        <class class="row">
+            <div class="col-md-4 mb-2">
+                <!-- card -->
+                <div class="card ">
+                    <iframe width="350" height="315" src="https://www.youtube.com/embed/Zqc_lc93hak" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="card-body card text-white bg-dark">
+                        <h3 class="card-text">Latihan#1 </h3>
+                        <h3 class="card-text">Squat</h3>
+                        <h7 class="card-text">Sumber : https://youtu.be/Zqc_lc93hak
+                        </h7>
+                    </div>
+                </div>
+
+            </div>
+            <!-- akhir card -->
+
+            <div class="col-md-4 mb-4">
+                <!-- card -->
+                <div class="card">
+                    <iframe width="350" height="315" src="https://www.youtube.com/embed/UpyDdQjBTa0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="card-body card text-white bg-dark">
+                        <h3 class="card-text">Latihan#2 </h3>
+                        <h3 class="card-text"> Lunges</h3>
+                        <h7 class="card-text">Sumber : https://youtu.be/UpyDdQjBTa0
+                        </h7>
+                    </div>
+                </div>
+                <!-- akhir card -->
+            </div>
+            <div class="col-md-4 mb-4">
+                <!-- card -->
+                <div class="card">
+                    <iframe width="350" height="315" src="https://www.youtube.com/embed/UV8gOrHmuKc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="card-body card text-white bg-dark">
+                        <h3 class="card-text">Latihan#3 </h3>
+                        <h3 class="card-text">Calf Raises </h3>
+                        <h7 class="card-text">Sumber : https://youtu.be/UV8gOrHmuKc
+                        </h7>
+                    </div>
+                </div>
+                <!-- akhir card -->
+            </div>
+            <div class="col-md-4 mb-4">
+                <!-- card -->
+                <div class="card">
+                    <iframe width="350" height="315" src="https://www.youtube.com/embed/gWHQpMUd7vw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="card-body card text-white bg-dark">
+                        <h3 class="card-text">Latihan#4 </h3>
+                        <h3 class="card-text">Shoulder Tap </h3>
+                        <h7 class="card-text">Sumber : https://youtu.be/gWHQpMUd7vw
+                        </h7>
+                    </div>
+                </div>
+                <!-- akhir card -->
+            </div>
+            <div class="col-md-4 mb-4">
+                <!-- card -->
+                <div class="card">
+                    <iframe width="350" height="315" src="https://www.youtube.com/embed/1XOP5TwkVBk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="card-body card text-white bg-dark">
+                        <h3 class="card-text">Latihan#5 </h3>
+                        <h3 class="card-text">Prone Revese fly </h3>
+                        <h7 class="card-text">Sumber :
+                            <br>
+                            https://youtu.be/1XOP5TwkVBk
+                        </h7>
+                    </div>
+                </div>
+                <!-- akhir card -->
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <!-- card -->
+                <div class="card">
+                    <iframe width="350" height="315" src="https://www.youtube.com/embed/WRnq49TAv-w" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="card-body card text-white bg-dark">
+                        <h3 class="card-text">Latihan#6 </h3>
+                        <h3 class="card-text">Flutter Kick </h3>
+                        <h7 class="card-text">Sumber :
+                            <br>
+                            https://youtu.be/WRnq49TAv-w
+                        </h7>
+                    </div>
+                </div>
+                <!-- akhir card -->
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <!-- card -->
+                <div class="card">
+                    <iframe width="350" height="315" src="https://www.youtube.com/embed/xMtCZE6rEiA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="card-body card text-white bg-dark">
+                        <h3 class="card-text">Latihan#7 </h3>
+                        <h3 class="card-text">Side Bridges </h3>
+                        <h7 class="card-text">Sumber :
+                            <br>
+                            https://youtu.be/xMtCZE6rEiA
+                        </h7>
+                    </div>
+                </div>
+                <!-- akhir card -->
+            </div>
+            <div class="col-md-4 mb-4">
+                <!-- card -->
+                <div class="card">
+                    <iframe width="350" height="315" src="https://www.youtube.com/embed/H8RrfDOLiZU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="card-body card text-white bg-dark">
+                        <h3 class="card-text">Latihan#8 </h3>
+                        <h3 class="card-text">Side Leg Raises</h3>
+                        <h7 class="card-text">Sumber :
+                            <br>
+                            https://youtu.be/H8RrfDOLiZU
+                        </h7>
+                    </div>
+                </div>
+                <!-- akhir card -->
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <!-- card -->
+                <div class="card">
+                    <iframe width="350" height="315" src="https://www.youtube.com/embed/v9LABVJzv8A" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="card-body card text-white bg-dark">
+                        <h3 class="card-text">Latihan#9 </h3>
+                        <h3 class="card-text">PushUp</h3>
+                        <h7 class="card-text">Sumber :
+                            <br>
+                            https://youtu.be/v9LABVJzv8A
+                        </h7>
+                    </div>
+                </div>
+                <!-- akhir card -->
+            </div>
+
+
+
+
+
+            <!-- akhir content -->
+
+
+
+
+            <!-- Optional JavaScript -->
+            <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+            <script type="text/javascript" src="js/jquery-3.4.1.slim.min.js"></script>
+            <script type="text/javascript" src="js/popper.min.js"></script>
+            <script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
